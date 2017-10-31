62b3cb2db16be642a1d16b0762cffe3add2967e3
mlazear10 <mlazear10@icloud.com>
2017-10-31 16:32:05 -0700

    updated files

---
+++ bamazon.js
@@ -0,0 +1,71 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: "",
+    database: "bamazon"
+})
+
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("connection successful");
+    makeTable();
+})
+
+var makeTable = function() {
+    connection.query("SELECT * FROM products", function(err, res){
+        for(var i=0; i< res.length; i++) {
+            console.log(res[i].itemid+" || "+res[i].productName+" || "+res[i].departmentName+" || "+res[i].stockQuantity+" || "+res[i].price+"\n");
+        }
+    promptCustomer(res);
+    })
+}
+
+var promptCustomer = function(res) {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'choice',
+        message: "What would you like to purchase? [Quit with Q]"
+    }]).then(function(answer){
+        var correct = false;
+        if (answer.choice.toUpperCase() == "Q") {
+            process.exit;
+        }
+        for (var i = 0; i < res.length; i++) {
+            if(res[i].productName == answer.choice) {
+                correct=true;
+                var product = answer.choice;
+                var id = i;
+                inquirer.prompt({
+                    type: "input",
+                    name: "quantity",
+                    message: "How many would you like to buy?",
+                    validate: function(value) {
+                        if(isNaN(value)==false) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then(function(answer){
+                    if((res[id].stockQuantity-answer.quantity)>0) {
+                        connection.query("UPDATE products SET stockQuantity='"+(res[id].stockQuantity-answer.quant)+"' WHERE productName='"+product+"'",function(err,res2){
+                            console.log("Product Bought");
+                            makeTable();
+                        })
+                    } else {
+                        console.log("Not a valid selection");
+                        promptCustomer(res);
+                    }
+                })
+            }
+        }
+    if(i==res.length && correct == false) {
+        console.log("Not a valid selection");
+        promptCustomer(res);
+    }
+    })
+}
---
+++ bamazon.sql
@@ -0,0 +1,22 @@
+drop database bamazon;
+create database bamazon;
+use bamazon;
+
+create table products(
+	itemid integer auto_increment not null,
+    productName varchar(50) not null,
+    departmentName varchar(50) not null,
+    price decimal(10,4) not null,
+    stockQuantity integer(10) not null,
+    primary key (itemid)
+);
+    
+INSERT INTO products(productName, departmentName, price, stockQuantity)
+VALUES ("Gears of War", "video games", 30.99, 56),
+ ("apples", "food", 0.49, 1000), 
+ ("coffee mug", "kitchenware", 7.85, 500),
+ ("bikes", "sporting goods", 350.50, 40),
+ ("monopoly", "board game", 10.4, 45),
+ ("Mad Max", "film", 20, 100);
+ 
+ select * from bamazon.products;
